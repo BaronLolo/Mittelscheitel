39a149223c392e135e130e64e1df2d89939659ef
s4miiii <fabbros1@edu.htl-villach.at>
2021-06-28 08:48:08 +0200

    finished UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,20 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
 
 
+    @FXML
+    private void colorPicker(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnKaufenClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnAdminClicked(ActionEvent actionEvent) {
+    }
 }
