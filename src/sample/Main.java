7582d56a42f0d9d08505b93a94dde042996ccb18
s4miiii <fabbros1@edu.htl-villach.at>
2021-06-28 10:06:56 +0200

    added some new gui features

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Car Management");
+        primaryStage.setTitle("Car Shop");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
